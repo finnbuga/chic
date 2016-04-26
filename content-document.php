4691c1fd2857572896d75b5b5b684f64ed71f7f6
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-04-26 10:38:03 +0100

    Restrict access to documents

--- content-document.php
+++ content-document.php
@@ -1,7 +1,11 @@
 <article class="document <?php print implode( ' ', otm_get_all_terms_ids( get_the_ID() )); ?>">
 	<header class="document-header">
 		<h1 class="document-title">
-			<a href="<?php print otm_document_get_attachment_url(); ?>" rel="bookmark"><?php the_title(); ?></a>
+			<?php if ( current_user_can( 'read_private_posts' ) ): ?>
+				<a href="<?php print otm_document_get_attachment_url(); ?>"><?php the_title(); ?></a>
+			<?php else: ?>
+				<a onclick="alert('Members only.');"><?php the_title(); ?></a>
+			<?php endif; ?>
 		</h1>
 	</header>
 
