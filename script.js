4abc860516cdfd7588a90c70657744456d313cea
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-05-03 10:39:22 +0100

    Auto-submit form

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 (function($) {
     $(function(){
+        var filters = $('.filters-select');
+
+        // On change reset all other filters and auto-submit form
+        filters.change( function() {
+            var value = $(this).val();
+            $('.filters-select').val('');
+            $(this).val(value);
+
+            $(this).closest('form').submit();
+        });
+
         // Makes select elements nicer using Selectric library
-        $('.filters-select').selectric({
+        filters.selectric({
             maxHeight: 400
         });
     });
