6924eadbd7c6c9934543ac2a98049a5f0fff672c
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-08-18 15:34:44 +0100

    New feature: cleanup the widgets list

--- functions.php
+++ functions.php
@@ -74,3 +74,22 @@ function otm_manage_sidebars(){
 		'after_title' => '</h3>',
 	) );
 }
+
+/**
+ * Cleanup the widgets list
+ */
+add_action( 'widgets_init', 'otm_unregister_default_widgets', 11 );
+function otm_unregister_default_widgets() {
+	unregister_widget( 'WP_Widget_Pages' );
+	unregister_widget( 'WP_Widget_Calendar' );
+	unregister_widget( 'WP_Widget_Archives' );
+	unregister_widget( 'WP_Widget_Links' );
+	unregister_widget( 'WP_Widget_Meta' );
+	unregister_widget( 'WP_Widget_Search' );
+	unregister_widget( 'WP_Widget_Categories' );
+	unregister_widget( 'WP_Widget_Recent_Posts' );
+	unregister_widget( 'WP_Widget_Recent_Comments' );
+	unregister_widget( 'WP_Widget_RSS' );
+	unregister_widget( 'WP_Widget_Tag_Cloud' );
+	unregister_widget( 'WP_Nav_Menu_Widget' );
+}
