0450ba21b74426413974c237808b8c6b24c76062
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-10-13 14:18:53 +0100

    Add Manager and Member roles

--- functions-backend.php
+++ functions-backend.php
@@ -4,6 +4,39 @@
  */
 
 /**
+ * Add Manager and Member roles
+ */
+add_action( 'after_switch_theme', 'otm_documents_add_roles' );
+function otm_documents_add_roles() {
+	$editor_capabilities  = get_role( 'editor' )->capabilities;
+	$manager_capabilities = array_merge(
+		$editor_capabilities,
+		array(
+			'list_users'           => true,
+			'create_users'         => true,
+			'add_users'            => true,
+			'edit_users'           => true,
+			'delete_users'         => true,
+			'remove_users'         => true,
+			'promote_users'        => true,
+			'manage_network_users' => true,
+		)
+	);
+	//remove_role('manager'); // If role exists it will not be overridden
+	add_role( 'manager', __( 'Manager' ), $manager_capabilities );
+
+	$subscriber_capabilities = get_role( 'subscriber' )->capabilities;
+	$member_capabilities     = array_merge(
+		$subscriber_capabilities,
+		array(
+			'read_private_pages' => true
+		)
+	);
+	//remove_role('member'); // If role exists it will not be overridden
+	add_role( 'member', __( 'Member' ), $member_capabilities );
+}
+
+/**
  * Add PDF filter on the Media page
  */
 add_filter( 'post_mime_types', 'otm_add_pdf_filter' );
