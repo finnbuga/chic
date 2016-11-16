e7718bec4551a942c0994322899f541f90429e08
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-11-16 12:28:52 +0000

    Refactor: move functions to the right files

--- functions-backend.php
+++ functions-backend.php
@@ -37,6 +37,14 @@ function otm_documents_add_roles() {
 }
 
 /**
+ * Set default role
+ */
+add_action( 'after_switch_theme', 'otm_set_default_role' );
+function otm_set_default_role( $query ) {
+
+}
+
+/**
  * Add PDF filter on the Media page
  */
 add_filter( 'post_mime_types', 'otm_add_pdf_filter' );
@@ -199,41 +207,4 @@ function otm_customise_toolbar( WP_Admin_Bar $admin_bar ) {
 			$admin_bar->remove_node( 'view' );
 		}
 	}
-}
-
-/**
- * Move the Event taxonomy meta box from the side column to the main column
- */
-add_action( 'do_meta_boxes', 'otm_documents_change_event_meta_box_position' );
-function otm_documents_change_event_meta_box_position( $post_type ) {
-	$tax_name = 'event';
-
-	if ( $post_type != 'document' ) {
-		return;
-	}
-
-	remove_meta_box( $tax_name . 'div', $post_type, 'side' );
-
-	$taxonomy = get_taxonomy( $tax_name );
-	if ( ! $taxonomy->show_ui || false === $taxonomy->meta_box_cb ) {
-		return;
-	}
-
-	add_meta_box( $tax_name . 'box', $taxonomy->labels->singular_name, $taxonomy->meta_box_cb, null, 'advanced', 'core',
-		array( 'taxonomy' => $tax_name ) );
-}
-
-/**
- * Order Events taxonomy terms by term_id
- *
- * so that the last event added will be on the top of the list
- */
-add_filter( 'get_terms_args', 'otm_documents_reorder_events_by_term_id', 10, 2 );
-function otm_documents_reorder_events_by_term_id( $args, $taxonomies ) {
-	if ( 'event' == $taxonomies[0] ) {
-		$args['orderby'] = 'term_id';
-		$args['order']   = 'DESC';
-	}
-
-	return $args;
 }
